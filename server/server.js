50a2531399ae794044d695e0a5c740f922e57451
Sachitha Suresh <sachsure@iu.edu>
2021-11-01 18:04:28 -0400

    Changes for heroku deployment

---
+++ server/server.js
@@ -0,0 +1,40 @@
+//dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+const app = express();
+const path = require('path');
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+  
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("/*",(req,res)=>{
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+const port = process.env.PORT || 3000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Woohoo! The server is up and running on port ${port} !`));
